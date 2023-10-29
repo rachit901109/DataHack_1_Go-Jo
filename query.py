2259413730074cb84d65a0839e111778fb17851b
Alpha131 <varunpillai20@gmail.com>
2023-10-29 08:13:32 +0530

    chatbot functionality

--- query.py
+++ query.py
@@ -2,40 +2,16 @@ import streamlit as st
 import pandas as pd
 import os
 from pandasai import PandasAI
-from pandasai.llm import Falcon
+from pandasai.llm import GooglePalm  # Import GooglePalm
 from pandasai import SmartDataframe
 from dotenv import load_dotenv
 
-# Load OpenAI API key from .env file
+# Load Google Palm API key from .env file
 load_dotenv()
-hfat = os.getenv("HUGGINGFACE_API_KEY")
+google_palm_api_key = os.getenv("GOOGLE_PALM_API_KEY")
 
-# Instantiate LLM and PandasAI objects
-llm = Falcon(api_key=hfat, use_auth_token=True)
-
-
-# @st.cache(allow_output_mutation=True)
-# def initialize_falcon_model():
-#     llm = Falcon(api_token=hfat, use_Auth_token=True)
-#     return llm
-
-# llm = initialize_falcon_model()
-
-# Function to load data, cached to avoid re-execution
-# @st.cache(allow_output_mutation=True)
-# def load_data(file_path):
-#     try:
-#         return pd.read_csv(file_path)
-#     except Exception as e:
-#         st.error(f"Error loading data: {str(e)}")
-
-# Function to perform chatbot response
-# def chatbot_response(question, data):
-#     try:
-#         # Use pandas_ai to generate response
-#         return pandas_ai.run(data, question)
-#     except Exception as e:
-#         st.error(f"Error generating chatbot response: {str(e)}")
+# Instantiate LLM and PandasAI objects using GooglePalm
+llm = GooglePalm(api_key=google_palm_api_key)
 
 # Function to display data and chatbot response
 def display_data_and_response(data, question):
@@ -44,29 +20,35 @@ def display_data_and_response(data, question):
 
     st.subheader("Chatbot Response:")
     chatbot_reply = data.chat(question)
-    
-    # if chatbot_reply is none assign a default value of no answer
-    if chatbot_reply is None:
-        chatbot_reply = "Answer not found. Sorry!"
-    
-    print("this is reply",chatbot_reply)
-    st.markdown(f"_{chatbot_reply}_")
+    print(chatbot_reply)
+
+    # Check if chatbot_reply is a dictionary
+    # if isinstance(chatbot_reply, dict):
+    #     # Check if "type" is in the dictionary
+    #     if "type" in chatbot_reply:
+    #         # If chatbot_reply["type"] is None or empty, assign a default value of no answer
+    #         response_type = chatbot_reply["type"] or "Answer not found. Sorry!"
+    #         st.markdown(f"_{response_type}_")
+    #     else:
+    #         st.markdown("_Answer not found. Sorry!_")
+    # else:
+    #     st.markdown("_Answer not found. Sorry!_")
+
+    # Display chatbot reply in Streamlit
+    st.write("Chatbot Reply:", chatbot_reply)
 
 # Main function
 def query():
     st.title("DataWiz - Chatbot and Table")
 
-    # Load the data, cached to avoid re-execution
-    file_path = r"startup_funding_all_years.csv"
-    # df_startup = load_data(file_path)
+    # Load the data
+    file_path = "startup_funding_all_years.csv"
     df = pd.read_csv(file_path)
     df_startup = SmartDataframe(df, config={"llm": llm})
-    
-
 
     # Display the first few rows of the data and chatbot response
-    user_input = st.text_input("You:", "Type your question here...")
-
+    user_input = st.text_input("You:", "")
+    # user_input = st.text_input("You:", "")
     if user_input:
         display_data_and_response(df_startup, user_input)
 
